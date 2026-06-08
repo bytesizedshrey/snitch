3f2dc32c9765d37cf81ae38d48bbd63834c438ca
bytesizedshrey <shreyash18gajbhiye@icloud.com>
2026-06-09 03:28:48 +0530

    Refactor Vite configuration for improved readability and add detailed comments on server proxy setup for API requests.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()
-  ],
-  server:{
-  proxy:{
-    '/api':{
-      target:'http://localhost:3000',
-      changeOrigin: true,
-      secure : false,
-    }
-  }
-}
-})
+  plugins: [react(), tailwindcss()],
+  /**
+Proxy is basically a middleman that catches `/api` requests from frontend and silently forwards them to backend (`localhost:3000`) so frontend and backend vibe together without CORS drama.
+   */
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
