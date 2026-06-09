6efbffc5a18fe497e1b281ccf756397dbd4ba141
bytesizedshrey <shreyash18gajbhiye@icloud.com>
2026-06-10 02:50:07 +0530

    Add product routes to backend application for product management functionality.

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import authRouter from './routes/auth.routes.js'
 import cors from 'cors'
 import passport from'passport'
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
+import productRouter from './routes/product.routes.js'
 
 
 const app = express()
@@ -34,5 +35,6 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/api/auth', authRouter)
+app.use('/api/products', productRouter)
 
 export default app
