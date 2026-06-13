9e7ad4a864994e56f6570dcf75b9455296559ff5
bytesizedshrey <shreyash18gajbhiye@icloud.com>
2026-06-13 17:50:07 +0530

    fix: hardcode NODE_ENV to production to fix cookies

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -36,6 +36,6 @@ export const config = {
     JWT_SECRET : process.env.JWT_SECRET,
     GOOGLE_CLIENT_ID : process.env.GOOGLE_CLIENT_ID ,
     GOOGLE_CLIENT_SECRET : process.env.GOOGLE_CLIENT_SECRET,
-    NODE_ENV : process.env.NODE_ENV || 'development',
+    NODE_ENV : 'production',
     IMAGEKIT_PRIVATE_KEY : process.env.IMAGEKIT_PRIVATE_KEY
 } 
